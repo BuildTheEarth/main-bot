fe2b908d7f853775e96e45e96dba0ce491cee587
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-27 20:12:25 -0300

    refactor: simplify main function

--- index.js
+++ index.js
@@ -3,28 +3,25 @@ const Client = require("./struct/Client");
 const client = new Client();
 require("./modules/functions.js")();
 
-const init = async () => {
-    const cmdFiles = await fs.readdir("./commands/");
-    console.log(`Loading a total of ${cmdFiles.length} commands.`);
-    cmdFiles.forEach((f) => {
-        if (!f.endsWith(".js")) return;
-        const response = client.loadCommand(f);
-        if (response) console.log(response);
+async function main() {
+    const commands = await fs.readdir("./commands");
+    console.log(`Loading ${commands.length} commands...`);
+    commands.forEach(client.loadCommand);
+
+    const events = await fs.readdir("./events");
+    console.log(`Loading ${events.length} events...`);
+    events.forEach(event => {
+        const name = event.replace(/\.js$/, "");
+        const handler = require(`./events/${name}`);
+        client.on(name, handler.bind(null, client));
     });
 
-    const evtFiles = await fs.readdir("./events/");
-    console.log(`Loading a total of ${evtFiles.length} events.`);
-    evtFiles.forEach((file) => {
-        const eventName = file.split(".")[0];
-        console.log(`Loading Event: ${eventName}`);
-        const event = require(`./events/${file}`);
-        client.on(eventName, event.bind(null, client));
+    client.config.permLevels.forEach(level => {
+        client.levelCache[level.name] = level.level;
     });
 
-    for (const level of client.config.permLevels)
-        client.levelCache[thisLevel.name] = level.level;
+    await client.login(client.config.token);
+    console.log("Logged in!");
+}
 
-    client.login(client.config.token);
-};
-
-init();
+main();
