e1e07968f3c4a3f9e2061034dbd6b400597ecdfb
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 19:55:12 -0300

    chore(config): remove perm levels, ren. db.password -> db.pass, remove custom values

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
-const config = {
-    prefix: "=",
-    ownerIds: ["377551134702829568", "391984806638125066"],
+module.exports = {
+    prefix: "",
+    ownerIds: [],
     logChannel: "",
 
     token: "",
@@ -8,66 +8,6 @@ const config = {
         host: "",
         name: "",
         user: "",
-        password: "",
-    },
-
-    permLevels: [
-        {
-            level: 0,
-            name: "User",
-            check: () => true,
-        },
-
-        {
-            level: 2,
-            name: "Staff",
-            check: message =>
-                message.author.roles.cache.has(r => r.name == "Staff")
-                    ? true
-                    : false,
-        },
-
-        {
-            level: 6,
-            name: "Helper",
-            check: message =>
-                message.author.roles.cache.has(r => r.name == "Helper")
-                    ? true
-                    : false,
-        },
-
-        {
-            level: 7,
-            name: "Moderator",
-            check: message =>
-                message.author.roles.cache.has(r => r.name == "Moderator")
-                    ? true
-                    : false,
-        },
-
-        {
-            level: 8,
-            name: "Manager",
-            check: message =>
-                message.author.roles.cache.has(r => r.name == "Manager")
-                    ? true
-                    : false,
-        },
-
-        {
-            level: 9,
-            name: "Admin",
-            check: message =>
-                message.author.roles.cache.has(r => r.name == "Admin")
-                    ? true
-                    : false,
-        },
-
-        {
-            level: 10,
-            name: "Bot Developer",
-            check: message => config.ownerIds.includes(message.author.id),
-        },
-    ],
-};
-module.exports = config;
+        pass: ""
+    }
+}
