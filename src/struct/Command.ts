f41eafbc5bd28368ae4b49dd93ca47d65c63ed6c
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 15:28:32 -0300

    feat: create Command class

---
+++ src/struct/Command.ts
@@ -0,0 +1,28 @@
+import Discord from "discord.js"
+
+export default class Command implements CommandProperties {
+    name: string
+    aliases: string[]
+    description: string
+    permission: string
+    usage: string
+    run: (client: Discord.Client, message: Discord.Message, args: string) => void
+
+    constructor(properties: CommandProperties) {
+        this.name = properties.name
+        this.aliases = properties.aliases
+        this.description = properties.description
+        this.permission = properties.permission
+        this.usage = properties.usage
+        this.run = properties.run
+    }
+}
+
+export type CommandProperties = {
+    name: string
+    aliases: string[]
+    description: string
+    permission: string
+    usage: string
+    run: (client: Discord.Client, message: Discord.Message, args: string) => void
+}
