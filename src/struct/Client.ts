896c26a42772e9cae2c8d81548b16f444b9f3616
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 20:10:56 -0300

    feat: create ConfigManager class

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -1,12 +1,12 @@
-import config from "../../config"
 import Discord from "discord.js"
 import EventList from "./client/EventList"
 import CommandList from "./client/CommandList"
+import ConfigManager from "./client/ConfigManager"
 import createLogger from "@buildtheearth/bot-logger"
 
 export default class Client /**/ extends Discord.Client {
     logger = createLogger()
-    config = config
+    config = new ConfigManager()
     events = new EventList(this)
     commands = new CommandList()
     aliases = new Discord.Collection()
---
+++ src/struct/client/ConfigManager.ts
@@ -0,0 +1,31 @@
+import path from "path"
+
+// TODO: use YAML
+export default class ConfigManager implements Config {
+    prefix: string
+    ownerIds: string[]
+    logChannel: string
+    token: string
+    db: { host: string; name: string; user: string; pass: string }
+
+    async load() {
+        const config: Config = await import(path.join(__dirname, "../../../config"))
+        for (const [key, value] of Object.entries(config)) this[key] = value
+    }
+}
+
+export type ConfigKey = "prefix" | "ownerIds" | "logChannel" | "token" | "db"
+
+export type Config = {
+    prefix: string
+    ownerIds: string[]
+    logChannel: string
+
+    token: string
+    db: {
+        host: string
+        name: string
+        user: string
+        pass: string
+    }
+}
