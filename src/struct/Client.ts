ca6b564f2bf7d577458a16ea44e26feb3239a376
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 20:21:27 -0300

    feat(Client): create db and initDatabase()

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -1,3 +1,4 @@
+import TypeORM from "typeorm"
 import Discord from "discord.js"
 import EventList from "./client/EventList"
 import CommandList from "./client/CommandList"
@@ -5,9 +6,22 @@ import ConfigManager from "./client/ConfigManager"
 import createLogger from "@buildtheearth/bot-logger"
 
 export default class Client /**/ extends Discord.Client {
+    db: TypeORM.Connection
     logger = createLogger()
     config = new ConfigManager()
     events = new EventList(this)
     commands = new CommandList()
     aliases = new Discord.Collection()
+
+    async initDatabase() {
+        this.db = await TypeORM.createConnection({
+            type: "mysql",
+            host: this.config.db.host,
+            database: this.config.db.name,
+            username: this.config.db.name,
+            password: this.config.db.pass,
+            entities: [__dirname + "/../entities/*.js"],
+            synchronize: true
+        })
+    }
 }
