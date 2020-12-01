a864c6edc6173c3859adfb3929894a5c619f770c
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 19:44:05 -0300

    refactor(Client): remove clean(), levelCache and prefix

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -1,5 +1,3 @@
-import sql from "../modules/sql"
-import util from "util"
 import config from "../../config"
 import Discord from "discord.js"
 import EventList from "./EventList"
@@ -7,27 +5,9 @@ import CommandList from "./CommandList"
 import createLogger from "@buildtheearth/bot-logger"
 
 export default class Client /**/ extends Discord.Client {
-    sql = sql
     logger = createLogger()
     config = config
     events = new EventList(this)
     commands = new CommandList()
     aliases = new Discord.Collection()
-    levelCache = {}
-
-    get prefix() {
-        return this.config.prefix
-    }
-
-    async clean(client: this, text: any) {
-        if (text && text.constructor.name == "Promise") text = await text
-        if (typeof text !== "string") text = require("util").inspect(text, { depth: 1 })
-
-        text = text
-            .replace(/`/g, "`" + String.fromCharCode(8203))
-            .replace(/@/g, "@" + String.fromCharCode(8203))
-            .replace(this.token, "why u tryna steal token")
-
-        return text
-    }
 }
