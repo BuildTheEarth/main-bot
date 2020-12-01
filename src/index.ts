cf692bb476f27a654d25983de66e0e1aff897ffc
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 20:26:31 -0300

    refactor(index): use all new Client methods

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import fs from "fs"
 import Discord from "discord.js"
 import Client from "./struct/Client"
 import GuildMember from "./struct/GuildMember"
@@ -7,26 +6,26 @@ Discord.Structures.extend("GuildMember", () => GuildMember)
 const client = new Client()
 
 async function main() {
-    const commands = await fs.promises.readdir("./commands")
-    console.log(`Loading ${commands.length} commands...`)
-    commands.forEach(command => {
-        client.loadCommand(command)
-    })
+    client.logger.debug("Loading config...")
+    await client.config.load()
+    client.logger.info("Loaded config.")
 
-    const events = await fs.promises.readdir("./events")
-    console.log(`Loading ${events.length} events...`)
-    events.forEach(event => {
-        const name = event.replace(/\.js$/, "")
-        const handler = require(`./events/${name}`)
-        client.on(name, handler.bind(null, client))
-    })
+    client.logger.debug("Loading commands...")
+    await client.commands.load()
+    client.logger.info("Loaded commands.")
 
-    client.config.permLevels.forEach(level => {
-        client.levelCache[level.name] = level.level
-    })
+    client.logger.debug("Registering events...")
+    await client.events.load()
+    client.events.register()
+    client.logger.info("Registered events.")
 
-    await client.login(client.config.token)
-    console.log("Logged in!")
+    client.logger.debug("Connecting to database...")
+    await client.initDatabase()
+    client.logger.info("Connected to database.")
+
+    client.logger.debug("Logging in to Discord...")
+    await client.login()
+    client.logger.info("Logged in to Discord.")
 }
 
 main()
