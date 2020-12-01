5e6960bb050b478f6fae02dbe3b540b42bc9e77c
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 18:53:49 -0300

    feat: create ActionLog typeorm entity

---
+++ src/entities/ActionLog.ts
@@ -0,0 +1,37 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    BaseEntity,
+} from "typeorm"
+
+@Entity()
+export default class ActionLog extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    action: "warn" | "mute" | "kick" | "ban" | "unmute" | "unban"
+
+    @Column({ length: 18 })
+    member: string
+
+    @Column({ length: 18 })
+    executor: string
+
+    @Column({ length: 500 })
+    reason: string
+
+    @Column({ nullable: true })
+    length: number
+
+    @CreateDateColumn()
+    timestamp: Date
+
+    @Column({ length: 18 })
+    channel: string
+
+    @Column({ length: 18 })
+    message: string
+}
