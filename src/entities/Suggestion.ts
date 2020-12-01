eb77c2360b1a9ad24e595a654e3bd31dd3e75470
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 19:15:02 -0300

    feat: create Suggestion entity

---
+++ src/entities/Suggestion.ts
@@ -0,0 +1,28 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    BaseEntity,
+} from "typeorm"
+
+@Entity()
+export default class Suggestion extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ length: 18 })
+    author: string
+
+    @Column({ length: 2048 })
+    body: string
+
+    @CreateDateColumn()
+    timestamp: Date
+
+    @Column({ length: 18 })
+    message: string
+
+    @Column()
+    staff: boolean
+}
