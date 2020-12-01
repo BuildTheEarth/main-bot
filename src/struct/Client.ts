03d244d448e6cd35ed6aaece27f002e2d76e9dde
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 16:31:44 -0300

    feat(Client): create logger property

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -4,9 +4,11 @@ import config from "../../config"
 import Discord from "discord.js"
 import EventList from "./EventList"
 import CommandList from "./CommandList"
+import createLogger from "@buildtheearth/bot-logger"
 
 export default class Client /**/ extends Discord.Client {
     sql = sql
+    logger = createLogger()
     config = config
     events = new EventList(this)
     commands = new CommandList()
