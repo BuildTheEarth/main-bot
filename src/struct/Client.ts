096080142a24e93c6421a852c353e9e31224967c
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 16:18:10 -0300

    refactor(Client): remove permlevel()

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -17,24 +17,6 @@ export default class Client /**/ extends Discord.Client {
         return this.config.prefix
     }
 
-    async permlevel(message: Discord.Message) {
-        let permlvl = 0
-        const permOrder = this.config.permLevels
-            .slice(0)
-            .sort((p, c) => (p.level < c.level ? 1 : -1))
-
-        while (permOrder.length) {
-            const currentLevel = permOrder.shift()
-            if (message.guild && currentLevel.guildOnly) continue
-            if (currentLevel.check(message)) {
-                permlvl = currentLevel.level
-                break
-            }
-        }
-        return permlvl
-    }
-
-    // ???????????????????????????????????????????????
     async clean(client: this, text: any) {
         if (text && text.constructor.name == "Promise") text = await text
         if (typeof text !== "string") text = require("util").inspect(text, { depth: 1 })
