abc40b31ba7aff83e93d275840e62f8b6b622a0e
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 23:41:46 -0300

    feat(GuildMember): create mute() and unmute()

--- src/struct/discord/GuildMember.ts
+++ src/struct/discord/GuildMember.ts
@@ -1,11 +1,26 @@
 import Discord from "discord.js"
+import Client from "../Client"
+import Guild from "./Guild"
 
 export default class GuildMember extends Discord.GuildMember {
+    client: Client
+    guild: Guild
+
     hasStaffPermission(roles: string | string[]) {
         if (roles === "any") return true
         if (typeof roles === "string") roles = [roles]
         for (const role of roles)
             if (this.roles.cache.find(r => r.name === role)) return true
         return false
+    }
+
+    async mute(reason: string): Promise<this> {
+        await this.roles.add(this.guild.muteRole, reason)
+        return this
+    }
+
+    async unmute(reason: string): Promise<this> {
+        await this.roles.remove(this.guild.muteRole, reason)
+        return this
     }
 }
