e10fdb22e71b50620541b3f7be2666b53d9861f3
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-02 01:17:01 -0300

    refactor(commands/eval): rewrite, move to typescript

---
+++ src/commands/eval.ts
@@ -0,0 +1,24 @@
+import Discord from "discord.js"
+import Client from "../struct/Client"
+import Command from "../struct/Command"
+import roles from "../util/roles"
+
+export default new Command({
+    name: "eval",
+    aliases: ["run"],
+    description: "Evaluate JavaScript code.",
+    permission: [roles.BOT_DEVELOPER],
+    usage: "<code>",
+    async run(_client: Client, message: Discord.Message, args: string) {
+        const code = args.replace(/(^`(``)?(js)?|`(``)?$)/, "")
+        try {
+            const out = String(await eval(code)) || "\u200B"
+            const truncated = out.length > 1992 ? out.slice(0, 1989) + "..." : out
+            message.channel.send(`\`\`\`js\n${truncated}\n\`\`\``)
+        } catch (error) {
+            const msg = error.message || "\u200B"
+            const truncated = msg.length > 1994 ? msg.slice(0, 1991) + "..." : msg
+            message.channel.send(`\`\`\`${truncated}\`\`\``)
+        }
+    }
+})
