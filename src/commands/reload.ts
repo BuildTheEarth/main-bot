625a051e7962c0010e2d838a9f1393e34a70319d
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-02 05:57:30 -0300

    refactor(commands/reload): rewrite in typescript

---
+++ src/commands/reload.ts
@@ -0,0 +1,34 @@
+import Discord from "discord.js"
+import Client from "../struct/Client"
+import Command from "../struct/Command"
+import roles from "../util/roles"
+
+export default new Command({
+    name: "reload",
+    aliases: [],
+    description: "Reload a command.",
+    permission: roles.BOT_DEVELOPER,
+    usage: "<command>",
+    async run(client: Client, message: Discord.Message, args: string) {
+        const commandName = args.split(/ +/)[0]
+        const command = client.commands.search(commandName.toLowerCase())
+        if (!command) {
+            return message.channel.send({
+                embed: {
+                    color: client.config.colors.error,
+                    description: `Unknown command \`${commandName}\``
+                }
+            })
+        }
+
+        client.commands.unloadOne(command.name)
+        await client.commands.loadOne(command.name)
+
+        message.channel.send({
+            embed: {
+                color: client.config.colors.success,
+                description: `Reloaded command \`${command.name}\``
+            }
+        })
+    }
+})
