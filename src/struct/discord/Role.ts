e40d23d2222c6739bc34251888255010e1ab793b
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 23:40:08 -0300

    fix(Role): override properties to be of extended classes

--- src/struct/discord/Role.ts
+++ src/struct/discord/Role.ts
@@ -1,7 +1,14 @@
 import chalk from "chalk"
 import Discord from "discord.js"
+import Client from "../Client"
+import Guild from "./Guild"
+import GuildMember from "./GuildMember"
 
 export default class Role extends Discord.Role {
+    client: Client
+    guild: Guild
+    members: Discord.Collection<string, GuildMember>
+
     format(): string {
         // "#000000" is not black, but the default role color
         const color = this.hexColor === "#000000" ? chalk.white : chalk.hex(this.hexColor)
