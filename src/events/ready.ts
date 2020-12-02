fd4186654ff8fc663c1bc834564afbdfb93caa0f
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-02 00:00:30 -0300

    fix(events/ready): type 'this' parameter

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,7 @@
 import TimedPunishment from "../entities/TimedPunishment"
+import Client from "../struct/Client"
 
-export default async function ready() {
+export default async function ready(this: Client) {
     this.user.setActivity(`${this.config.prefix}help`, { type: "PLAYING" })
 
     const punishments = await TimedPunishment.find()
