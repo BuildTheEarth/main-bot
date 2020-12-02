92c008c91f9a7b380c6e8ca2d1a59b2a714bd4bd
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 22:47:33 -0300

    fix(Client): fix TypeORM imports

--- src/struct/Client.ts
+++ src/struct/Client.ts
@@ -1,4 +1,4 @@
-import TypeORM from "typeorm"
+import { Connection, createConnection } from "typeorm"
 import Discord from "discord.js"
 import EventList from "./client/EventList"
 import CommandList from "./client/CommandList"
@@ -6,7 +6,7 @@ import ConfigManager from "./client/ConfigManager"
 import createLogger from "@buildtheearth/bot-logger"
 
 export default class Client /**/ extends Discord.Client {
-    db: TypeORM.Connection
+    db: Connection
     logger = createLogger({ filePath: __dirname + "/../../logs/" })
     config = new ConfigManager()
     events = new EventList(this)
@@ -14,7 +14,7 @@ export default class Client /**/ extends Discord.Client {
     aliases = new Discord.Collection()
 
     async initDatabase() {
-        this.db = await TypeORM.createConnection({
+        this.db = await createConnection({
             type: "mysql",
             host: this.config.db.host,
             database: this.config.db.name,
