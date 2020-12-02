ed9be004e892e35f81bd3e04b76109f882b06876
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-02 06:41:51 -0300

    refactor(commands/mute): rewrite in typescript

---
+++ src/commands/mute.ts
@@ -0,0 +1,78 @@
+import Discord from "discord.js"
+import Client from "../struct/Client"
+import GuildMember from "../struct/discord/GuildMember"
+import TimedPunishment from "../entities/TimedPunishment"
+import Command from "../struct/Command"
+import roles from "../util/roles"
+import formatPunishmentTime from "../util/formatPunishmentTime"
+import ms from "ms"
+
+export default new Command({
+    name: "mute",
+    aliases: [],
+    description: "Mute a member.",
+    permission: [roles.HELPER, roles.MODERATOR, roles.BOT_DEVELOPER],
+    usage: "<member> <length> <reason>",
+    async run(client: Client, message: Discord.Message, args: string) {
+        const targetTag = args.match(/.{2,32}#\d{4}/)?.[0]
+        const targetID = args.match(/\d{18}/)?.[0]
+        const target = <GuildMember>(
+            (targetID
+                ? await message.guild.members.fetch({ user: targetID, cache: true })
+                : message.guild.members.cache.find(m => m.user.tag === targetTag))
+        )
+        if (targetID) args = args.split(" ").slice(1).join(" ").trim()
+        else args = args.replace(targetTag, "").trim()
+
+        if (!target) {
+            return message.channel.send({
+                embed: {
+                    color: client.config.colors.error,
+                    description: `Couldn't find user \`${targetTag || targetID}\``
+                }
+            })
+        }
+
+        if (target.hasStaffPermission(roles.STAFF)) {
+            return message.channel.send({
+                embed: {
+                    color: client.config.colors.error,
+                    description: "You can't mute other staff!"
+                }
+            })
+        }
+
+        const length = ms(args.split(" ")[0]) || Infinity
+        const reason = args.split(" ").slice(1).join(" ").trim()
+        const punishment = new TimedPunishment()
+        punishment.user = target.id
+        punishment.type = "mute"
+        punishment.end = Date.now() + length
+
+        await target.mute(reason)
+        await punishment.save()
+        setTimeout(async () => {
+            await punishment.undo(this)
+            await punishment.remove()
+        }, length)
+
+        const formattedLength = formatPunishmentTime(length)
+
+        message.channel.send({
+            embed: {
+                color: client.config.colors.success,
+                description:
+                    target.id !== message.author.id
+                        ? `Muted \`${target.user.tag}\` ${formattedLength} • *${reason}*`
+                        : `Muting yourself, huh? • *${reason}*`
+            }
+        })
+
+        target.user.send({
+            embed: {
+                color: client.config.colors.error,
+                description: `${message.author} has muted you ${formattedLength}:\n\n*${reason}*`
+            }
+        })
+    }
+})
