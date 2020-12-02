9d182354871b9ea8596db09feb1d522e80089e52
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-02 01:38:53 -0300

    refactor(commands/(un)lock): rewrite in typescript

---
+++ src/commands/lock.ts
@@ -0,0 +1,29 @@
+import Discord from "discord.js"
+import Client from "../struct/Client"
+import Command from "../struct/Command"
+import roles from "../util/roles"
+
+export default new Command({
+    name: "lock",
+    aliases: [],
+    description: "Lock the channel.",
+    permission: [roles.MANAGER, roles.BOT_DEVELOPER],
+    usage: "[channel]",
+    async run(client: Client, message: Discord.Message, args: string) {
+        const channelID = args.match(/\d{18}/)?.[0]
+        const channel = <Discord.TextChannel>(
+            (message.guild.channels.cache.get(channelID) || message.channel)
+        )
+        await channel.updateOverwrite(
+            message.guild.id,
+            { SEND_MESSAGES: false },
+            `Locked by ${message.author.tag} (${message.author.id})`
+        )
+        message.channel.send({
+            embed: {
+                color: client.config.colors.success,
+                description: `Locked ${channel}.`
+            }
+        })
+    }
+})
