e5e3e13894008616ba46afa20763a45d5f8dde30
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-03 15:04:55 -0300

    feat(util): create truncateString()

---
+++ src/util/truncateString.ts
@@ -0,0 +1,7 @@
+export default function truncateString(string: string, length: number, ellipsis = "...") {
+    const exceeds = string.length > length
+    const truncatedLength = exceeds ? length - ellipsis.length : length
+    let truncated = string.slice(0, truncatedLength)
+    if (exceeds) truncated += ellipsis
+    return truncated
+}
