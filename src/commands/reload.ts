37823c6c1a7f1268c56673e210e0c48661682045
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-03 15:50:32 -0300

    feat(commands/reload): also support event handlers

--- src/commands/reload.ts
+++ src/commands/reload.ts
@@ -2,6 +2,7 @@ import Discord from "discord.js"
 import Client from "../struct/Client"
 import Command from "../struct/Command"
 import Roles from "../util/roles"
+import truncateString from "../util/truncateString"
 
 export default new Command({
     name: "reload",
@@ -10,24 +11,34 @@ export default new Command({
     permission: Roles.BOT_DEVELOPER,
     usage: "<command>",
     async run(client: Client, message: Discord.Message, args: string) {
-        const commandName = args.split(/ +/)[0]
-        const command = client.commands.search(commandName.toLowerCase())
-        if (!command) {
+        const name = args.split(/ +/)[0]
+        const command = client.commands.search(name)
+        const handler = client.events.get(name)
+        if (!command && !handler) {
+            const truncated = truncateString(name, 32, "...")
             return message.channel.send({
                 embed: {
                     color: client.config.colors.error,
-                    description: `Unknown command \`${commandName}\`.`
+                    description: `Unknown command or event handler \`${truncated}\`.`
                 }
             })
         }
 
-        client.commands.unloadOne(command.name)
-        await client.commands.loadOne(command.name)
+        if (handler) {
+            client.events.unregisterOne(name)
+            client.events.unloadOne(name)
+            await client.events.loadOne(name)
+            client.events.registerOne(name)
+        } else if (command) {
+            client.commands.unloadOne(command.name)
+            await client.commands.loadOne(command.name)
+        }
 
+        const type = command ? "command" : "event handler"
         message.channel.send({
             embed: {
                 color: client.config.colors.success,
-                description: `Reloaded command \`${command.name}\`.`
+                description: `Reloaded ${type} \`${name}\`.`
             }
         })
     }
