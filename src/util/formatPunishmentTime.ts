93deebe945a8d332b97adb75601afaf124836284
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-03 22:45:08 -0300

    fix(util/formatPunishmentTime): use 0 as perm.

--- src/util/formatPunishmentTime.ts
+++ src/util/formatPunishmentTime.ts
@@ -1,6 +1,6 @@
 import ms from "ms"
 
 export default function formatPunishmentTime(length: number): string {
-    if (length === Infinity) return "**permanently**"
+    if (length === 0) return "**permanently**"
     else return `for ${ms(length, { long: true })}`
 }
