348ef349e78cd2c716a8f9956d98664ef35e05ea
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-06 16:45:48 -0300

    feat(util/formatPunishmentTime): add optional 'bare' param

--- src/util/formatPunishmentTime.ts
+++ src/util/formatPunishmentTime.ts
@@ -1,6 +1,14 @@
 import ms from "ms"
 
-export default function formatPunishmentTime(length: number): string {
-    if (length === 0) return "**permanently**"
-    else return `for ${ms(length, { long: true })}`
+export default function formatPunishmentTime(
+    length: number,
+    bare: boolean = false
+): string {
+    if (bare) {
+        if (length === 0) return "**Permanent**"
+        else return ms(length, { long: true })
+    } else {
+        if (length === 0) return "**permanently**"
+        else return `for ${ms(length, { long: true })}`
+    }
 }
