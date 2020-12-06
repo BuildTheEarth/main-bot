e7e27ab602a289c14581f57c731a072354f66916
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-05 23:42:28 -0300

    feat(commands/case): create 'delete' subcommand, check if case was deleted

--- src/commands/case.ts
+++ src/commands/case.ts
@@ -1,3 +1,4 @@
+import Discord from "discord.js"
 import Client from "../struct/Client"
 import Message from "../struct/discord/Message"
 import Command from "../struct/Command"
@@ -11,36 +12,60 @@ export default new Command({
     description: "Check specific info on a case.",
     permission: [Roles.HELPER, Roles.MODERATOR],
     usage: "<id>",
+    subcommands: [
+        {
+            name: "delete",
+            description: "Delete a case.",
+            permission: Roles.MODERATOR,
+            usage: "<id>"
+        }
+    ],
     async run(this: Command, client: Client, message: Message, args: string) {
-        const id = Number(args.split(" ")[0])
+        const subcommand = args.split(/ +/)[0].toLowerCase()
+        args = args.split(" ").slice(1).join(" ").trim()
+
+        const id = ["edit", "delete"].includes(subcommand)
+            ? Number(args.split(" ")[0] || subcommand)
+            : Number(subcommand)
         if (Number.isNaN(id))
             return message.channel.sendError("You must provide a case ID!")
 
-        const log = await ActionLog.findOne(id)
-        if (!log)
-            return message.channel.sendError(`Couldn't find case with ID \`${id}\`.`)
+        const log = await ActionLog.findOne(id, { withDeleted: true })
+        if (!log) return message.channel.sendError(`Couldn't find case **#${id}**.`)
 
-        const messageLink = `https://discord.com/channels/${client.config.guilds.main}/${log.channel}/${log.message}`
-        // i'm so sorry
-        const timestamp = `${log.timestamp.getUTCDate()}/${log.timestamp.getUTCMonth()}/${log.timestamp.getUTCFullYear()} @ ${log.timestamp.getUTCHours()}:${log.timestamp.getUTCMinutes()}:${log.timestamp.getUTCSeconds()} UTC`
-        const embed = {
-            author: { name: `Case #${log.id} (${log.action})` },
-            fields: [
-                { name: "Member", value: `<@${log.member}>` },
-                { name: "Reason", value: log.reason },
-                { name: "Moderator", value: `<@${log.executor}>` },
-                { name: "Context", value: `[Link](${messageLink})` },
-                { name: "Time", value: timestamp }
-            ].map(f => ({ ...f, inline: true }))
+        if (!["edit", "delete"].includes(subcommand)) {
+            const messageLink = `https://discord.com/channels/${client.config.guilds.main}/${log.channel}/${log.message}`
+            // i'm so sorry
+            const time = log.createdAt
+            const timestamp = `${time.getUTCDate()}/${time.getUTCMonth()}/${time.getUTCFullYear()} @ ${time.getUTCHours()}:${time.getUTCMinutes()}:${time.getUTCSeconds()} UTC`
+            const embed: Discord.MessageEmbedOptions = {
+                author: { name: `Case #${log.id} (${log.action})` },
+                fields: [
+                    { name: "Member", value: `<@${log.member}>` },
+                    { name: "Reason", value: log.reason },
+                    { name: "Moderator", value: `<@${log.executor}>` },
+                    { name: "Context", value: `[Link](${messageLink})` },
+                    { name: "Time", value: timestamp }
+                ].map(field => ({ ...field, inline: true }))
+            }
+
+            if (log.length)
+                embed.fields.splice(1, 0, {
+                    name: "Length",
+                    value: ms(log.length, { long: true }),
+                    inline: true
+                })
+
+            const deleted = !!log.deletedAt
+            if (deleted) embed.description = "*This case has been deleted.*"
+
+            deleted
+                ? message.channel.sendError(embed)
+                : message.channel.sendSuccess(embed)
+        } else if (subcommand === "delete") {
+            if (!message.member.hasStaffPermission(Roles.MODERATOR)) return
+            await log.softRemove()
+            await message.channel.sendSuccess(`Deleted case **#${id}**.`)
         }
-
-        if (log.length)
-            embed.fields.splice(1, 0, {
-                name: "Length",
-                value: ms(log.length, { long: true }),
-                inline: true
-            })
-
-        message.channel.sendSuccess(embed)
     }
 })
