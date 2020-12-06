19a2dfcaf742f83cb70d47b5fb37be12512f06c1
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-05 21:46:17 -0300

    fix(util/roles): check for acronyms in role names

--- src/util/roles.ts
+++ src/util/roles.ts
@@ -1,6 +1,12 @@
+const knownAcronyms = ["PR", "BOTW", "DJ"]
+
 function get(_target: any, key: string): string {
     const words = key.split("_")
-    const capital = words.map(w => w[0].toUpperCase() + w.slice(1).toLowerCase())
+    const capital = words.map(word =>
+        knownAcronyms.includes(word)
+            ? word
+            : word[0].toUpperCase() + word.slice(1).toLowerCase()
+    )
     return capital.join(" ")
 }
 
