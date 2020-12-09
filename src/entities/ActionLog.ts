c80e26cff905191a039f0e1246cf95a3384fbc4f
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-08 23:15:57 -0300

    feat(entities/ActionLog): create send()

--- src/entities/ActionLog.ts
+++ src/entities/ActionLog.ts
@@ -8,7 +8,11 @@ import {
     OneToOne,
     JoinColumn
 } from "typeorm"
+import fecha from "fecha"
+import Discord from "discord.js"
+import TextChannel from "../struct/discord/TextChannel"
 import TimedPunishment from "./TimedPunishment"
+import formatPunishmentTime from "../util/formatPunishmentTime"
 
 export type Action = "warn" | "mute" | "kick" | "ban" | "unmute" | "unban"
 
@@ -57,5 +61,36 @@ export default class ActionLog extends BaseEntity {
         let formatted = `\` ${this.id}. \` ${this.reason}`
         if (this.deletedAt) formatted = `~~${formatted}~~`
         return formatted
+    }
+
+    async send(channel: TextChannel): Promise<Discord.Message> {
+        const messageLink = `https://discord.com/channels/${channel.client.config.guilds.main}/${this.channel}/${this.message}`
+        const utcOffset = this.createdAt.getTimezoneOffset() * 60000
+        const utc = new Date(this.createdAt.getTime() + utcOffset)
+        const timestamp = fecha.format(utc, "DD/MM/YY [@] hh:mm:ss UTC")
+        const length = this.length ? formatPunishmentTime(this.length, true) : "\u200B"
+        const embed: Discord.MessageEmbedOptions = {
+            author: { name: `Case #${this.id} (${this.action})` },
+            fields: [
+                { name: "Member", value: `<@${this.member}>` },
+                { name: this.length ? "Length" : "\u200B", value: length },
+                { name: "Reason", value: this.reason },
+                { name: "Moderator", value: `<@${this.executor}>` },
+                { name: "Context", value: `[Link](${messageLink})` },
+                { name: "Time", value: timestamp }
+            ].map(field => ({ ...field, inline: true }))
+        }
+
+        if (this.deletedAt) {
+            embed.description = "*This case has been deleted.*"
+            return await channel.sendError(embed)
+        }
+
+        if (this.punishment?.end > new Date()) {
+            const end = this.punishment.end.getTime() - Date.now()
+            embed.description = `*Ending in ${formatPunishmentTime(end, true)}.*`
+        }
+
+        return await channel.sendSuccess(embed)
     }
 }
