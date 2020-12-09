3743e5b97ef927bb1c58b0ade203bd3975f7701b
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-08 23:56:37 -0300

    feat(commands): create unban command

---
+++ src/commands/unban.ts
@@ -0,0 +1,47 @@
+import Client from "../struct/Client"
+import Message from "../struct/discord/Message"
+import Args from "../struct/Args"
+import TimedPunishment from "../entities/TimedPunishment"
+import ActionLog from "../entities/ActionLog"
+import Command from "../struct/Command"
+import Roles from "../util/roles"
+
+export default new Command({
+    name: "unban",
+    aliases: [],
+    description: "Unban a member.",
+    permission: Roles.MODERATOR,
+    usage: "<member> <reason>",
+    async run(this: Command, client: Client, message: Message, args: Args) {
+        const user = await args.consumeUser()
+        if (!user)
+            return message.channel.sendError(
+                user === undefined
+                    ? "You must provide a user to unban!"
+                    : "Couldn't find that user."
+            )
+
+        const reason = args.consumeRest()
+        if (!reason) return message.channel.sendError("You must provide a reason!")
+
+        const ban = await TimedPunishment.findOne({
+            where: { member: user.id, type: "ban" }
+        })
+        if (!ban) return message.channel.sendError("The user is not banned!")
+
+        await ban.undo(client)
+        await ban.remove()
+
+        const log = new ActionLog()
+        log.action = "unban"
+        log.member = user.id
+        log.executor = message.author.id
+        log.reason = reason
+        log.channel = message.channel.id
+        log.message = message.id
+        await log.save()
+
+        await message.channel.sendSuccess(`Unbanned ${user} (**#${log.id}**).`)
+        await client.log(log)
+    }
+})
