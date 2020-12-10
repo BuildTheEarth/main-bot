a151a2d043313fee3126138d38f58a27f2263518
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-10 18:07:33 -0300

    fix(decorators/Snowflake*Column): use 'varchar' type

--- src/entities/decorators/SnowflakeColumn.ts
+++ src/entities/decorators/SnowflakeColumn.ts
@@ -1,5 +1,5 @@
 import { Column, ColumnOptions } from "typeorm"
 
 export default function SnowflakeColumn(options?: ColumnOptions): PropertyDecorator {
-    return Column({ ...options, type: "string", length: "18" })
+    return Column({ ...options, type: "varchar", length: "18" })
 }
--- src/entities/decorators/SnowflakePrimaryColumn.ts
+++ src/entities/decorators/SnowflakePrimaryColumn.ts
@@ -3,5 +3,5 @@ import { PrimaryColumn, ColumnOptions } from "typeorm"
 export default function SnowflakePrimaryColumn(
     options?: ColumnOptions
 ): PropertyDecorator {
-    return PrimaryColumn({ ...options, type: "string", length: "18" })
+    return PrimaryColumn({ ...options, type: "varchar", length: "18" })
 }
