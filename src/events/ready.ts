ec607bbe17ec2e1cbf0c40b1f58de8a50a05d2f7
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-12 18:57:22 -0300

    refactor(events/ready): change activity to 'Playing with {count} members'

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,8 @@ import TimedPunishment from "../entities/TimedPunishment"
 import Client from "../struct/Client"
 
 export default async function ready(this: Client) {
-    this.user.setActivity(`${this.config.prefix}help`, { type: "PLAYING" })
+    const main = this.guilds.cache.get(this.config.guilds.main)
+    this.user.setActivity(`with ${main.memberCount}`, { type: "PLAYING" })
 
     const punishments = await TimedPunishment.find()
     for (const punishment of punishments) punishment.schedule(this)
