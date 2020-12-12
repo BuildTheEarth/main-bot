6fc83025346fa18114d40d7e77321663192289c8
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-12 19:16:19 -0300

    fix(events/ready): cache all reaction role messages

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,6 @@
 import TimedPunishment from "../entities/TimedPunishment"
 import Client from "../struct/Client"
+import TextChannel from "../struct/discord/TextChannel"
 
 export default async function ready(this: Client) {
     const main = this.guilds.cache.get(this.config.guilds.main)
@@ -7,4 +8,11 @@ export default async function ready(this: Client) {
 
     const punishments = await TimedPunishment.find()
     for (const punishment of punishments) punishment.schedule(this)
+
+    for (const channelID of Object.keys(this.config.reactionRoles)) {
+        const channel = (await this.channels.fetch(channelID, true)) as TextChannel
+        for (const messageID of Object.keys(this.config.reactionRoles[channelID])) {
+            await channel.messages.fetch(messageID)
+        }
+    }
 }
