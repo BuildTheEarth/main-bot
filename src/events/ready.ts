b88124828f50d81ce8455e67841806eb1c50bc15
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-12 22:33:24 -0300

    fix(events/ready): fix caching or whatever

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,19 +1,21 @@
 import TimedPunishment from "../entities/TimedPunishment"
 import Client from "../struct/Client"
 import TextChannel from "../struct/discord/TextChannel"
-import noop from "../util/noop"
 
 export default async function ready(this: Client) {
     const main = this.guilds.cache.get(this.config.guilds.main)
-    this.user.setActivity(`with ${main.memberCount}`, { type: "PLAYING" })
+    this.user.setActivity(`with ${main.memberCount} users`, { type: "PLAYING" })
 
     const punishments = await TimedPunishment.find()
     for (const punishment of punishments) punishment.schedule(this)
 
+    // cache reaction role messages
     for (const channelID of Object.keys(this.config.reactionRoles)) {
-        const channel = <TextChannel>await this.channels.fetch(channelID).catch(noop)
-        for (const messageID of Object.keys(this.config.reactionRoles[channelID])) {
-            await channel.messages.fetch(messageID).catch(noop)
+        const channel = await this.channels.fetch(channelID).catch(() => null)
+        if (channel) {
+            for (const messageID of Object.keys(this.config.reactionRoles[channelID])) {
+                await (<TextChannel>channel).messages.fetch(messageID).catch(() => null)
+            }
         }
     }
 }
