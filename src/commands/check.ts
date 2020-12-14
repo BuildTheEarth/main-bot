f136d5ebec83ce399ab884917b8c694a2de836bc
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-14 16:16:30 -0300

    refactor(commands/check): *only* show deleted cases when 'deleted' switch is enabled

--- src/commands/check.ts
+++ src/commands/check.ts
@@ -6,13 +6,14 @@ import Command from "../struct/Command"
 import Roles from "../util/roles"
 import ActionLog, { Action } from "../entities/ActionLog"
 import TimedPunishment from "../entities/TimedPunishment"
+import { FindManyOptions, Not } from "typeorm"
 
 export default new Command({
     name: "check",
     aliases: ["history", "logs", "records"],
     description: "Check a user's punishment records.",
     permission: [Roles.HELPER, Roles.MODERATOR],
-    usage: "<user> [deleted]",
+    usage: "<user> ['deleted']",
     async run(this: Command, client: Client, message: Message, args: Args) {
         const user = await args.consumeUser()
         const showDeleted = args.consume().toLowerCase() === "deleted"
@@ -24,13 +25,22 @@ export default new Command({
                     : "Couldn't find that user."
             )
 
-        const actionLogs = await ActionLog.find({
-            where: { member: user.id },
-            withDeleted: showDeleted
-        })
-        // prettier-ignore
-        const categorizedLogs: Record<Action, ActionLog[]> =
-            { warn: [], mute: [], kick: [], ban: [], unmute: [], unban: [] }
+        const criteria: FindManyOptions = { where: { member: user.id } }
+        if (showDeleted) {
+            // @ts-ignore: Property 'deletedAt' does not exist on type 'string'.
+            criteria.where.deletedAt = Not(null)
+            criteria.withDeleted = true
+        }
+
+        const actionLogs = await ActionLog.find(criteria)
+        const categorizedLogs: Record<Action, ActionLog[]> = {
+            warn: [],
+            mute: [],
+            kick: [],
+            ban: [],
+            unmute: [],
+            unban: []
+        }
 
         const clean = !actionLogs.length
         for (const log of actionLogs) categorizedLogs[log.action].push(log)
@@ -45,24 +55,21 @@ export default new Command({
         if (clean) {
             embed.description = `✨ No cases found for ${user} (${user.tag}).`
         } else {
-            const punishment = await TimedPunishment.findOne({
-                where: { member: user.id }
-            })
-            const adjective = punishment?.type === "mute" ? "muted" : "banned"
-            const log = actionLogs.find(log => log.punishment?.id === punishment?.id)
-            const hasOld = actionLogs.some(log => log.old)
+            const current = await TimedPunishment.findOne({ where: { member: user.id } })
+            const currentLog = actionLogs.find(log => log.punishment?.id === current?.id)
+            const adjective = current?.type === "mute" ? "muted" : "banned"
+            const cases = showDeleted ? "Cases" : "Deleted cases"
+            const attribute = showDeleted ? " deleted " : ""
 
-            embed.description = punishment
-                ? `${user} (${user.tag}) is currently ${adjective} (**#${log.id}**). Here are their cases:`
-                : `Cases for ${user} (${user.tag}):`
-
-            if (hasOld)
+            embed.description = current
+                ? `${user} (${user.tag}) is currently ${adjective} (**#${currentLog.id}**). Here are their${attribute}cases:`
+                : `${cases} for ${user} (${user.tag}):`
+            if (actionLogs.some(log => log.old))
                 embed.description += "\n(Cases older than 3 months are marked with \\📜)."
 
             for (const [action, logs] of Object.entries(categorizedLogs)) {
                 const actionTitle = action[0].toUpperCase() + action.slice(1) + "s"
-                const nonDeleted = logs.filter(log => !log.deletedAt)
-                const name = `${actionTitle} (${nonDeleted.length})`
+                const name = `${actionTitle} (${logs.length})`
                 const value = logs.map(log => log.format()).join("\n") || "\u200B"
                 embed.fields.push({ name, value, inline: true })
             }
