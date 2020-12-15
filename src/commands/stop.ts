69c0cfaea5c71418ccce7e345fba7db68849c6fd
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-15 20:36:53 -0300

    feat(commands): create stop command

---
+++ src/commands/stop.ts
@@ -0,0 +1,17 @@
+import Client from "../struct/Client"
+import Message from "../struct/discord/Message"
+import Args from "../struct/Args"
+import Command from "../struct/Command"
+import Roles from "../util/roles"
+
+export default new Command({
+    name: "stop",
+    aliases: ["close", "die", "bye", "adios", "shutdown", "off", "farewell"],
+    description: "Stop the bot.",
+    permission: Roles.BOT_DEVELOPER,
+    usage: "['bye']",
+    async run(client: Client, message: Message, args: Args) {
+        if (!args.consumeIf("bye")) await message.react("👋").catch(() => null)
+        process.exit(0)
+    }
+})
