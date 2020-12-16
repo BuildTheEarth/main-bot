76668fb26ecdb16b6d5a31b593c2296e1f07eab0
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-15 22:11:04 -0300

    feat(commands): create purge command

---
+++ src/commands/purge.ts
@@ -0,0 +1,22 @@
+import Client from "../struct/Client"
+import Message from "../struct/discord/Message"
+import Args from "../struct/Args"
+import Command from "../struct/Command"
+import Roles from "../util/roles"
+import TextChannel from "../struct/discord/TextChannel"
+
+export default new Command({
+    name: "purge",
+    aliases: ["prune", "bulkdelete"],
+    description: "Bulk-delete messages in a channel.",
+    permission: Roles.MODERATOR,
+    usage: "<amount>",
+    async run(this: Command, client: Client, message: Message, args: Args) {
+        const amount = Number(args.consume())
+        if (Number.isNaN(amount))
+            return message.channel.sendError("You must provide a valid amount!")
+
+        const purged = await (<TextChannel>message.channel).bulkDelete(amount, true)
+        await message.channel.sendSuccess(`Purged ${purged.size} messages.`)
+    }
+})
