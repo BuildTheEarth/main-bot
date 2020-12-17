de3ecb2f4b0a781ec92b8a3c16e31edf98013d73
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-17 00:42:16 -0300

    style(commands/catte): prettify (smh)

--- src/commands/catte.ts
+++ src/commands/catte.ts
@@ -5,7 +5,7 @@ import Roles from "../util/roles"
 
 export default new Command({
     name: "catte",
-    aliases: ["cAtte_","noah","arc"],
+    aliases: ["catte_", "noah", "arc"],
     description: "cAtte_ is the best",
     permission: Roles.ANY,
     usage: "",
