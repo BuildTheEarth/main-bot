234258d1fcff164df41cbe928b4b62fd044e0301
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-17 14:25:45 -0300

    fix/refactor(commands/language): change stuff

--- src/commands/language.ts
+++ src/commands/language.ts
@@ -3,52 +3,46 @@ import Message from "../struct/discord/Message"
 import Args from "../struct/Args"
 import Command from "../struct/Command"
 import Roles from "../util/roles"
-import noop from "../util/noop"
-import Guild from "../struct/discord/Guild" 
+
+const LANGUAGE_ROLES = {
+    english: "English",
+    espanol: "Español",
+    francais: "Français",
+    deutsch: "Deutsch",
+    russian: "Pусский",
+    portuguese: "Português",
+    italian: "Italiana",
+    international: "Ø",
+    chinese: "中文"
+}
 
 export default new Command({
     name: "language",
     aliases: ["lang"],
     description: "Give a member a language role.",
-    permission: Roles.SUPPORT, Roles.HELPER
-    usage: "<member> <lanuage>",
+    permission: [Roles.SUPPORT, Roles.HELPER],
+    usage: "<member> <language>",
     async run(this: Command, client: Client, message: Message, args: Args) {
         const user = await args.consumeUser()
-
         if (!user)
             return message.channel.sendError(
                 user === undefined
-                    ? "You must provide a user to give language roles too!"
+                    ? "You must provide a user to give a language role to!"
                     : "Couldn't find that user."
             )
 
-        const language = args.consumeRest()
-        let langmap = new Map([
-        ["english", "ENGLISH"],
-        ["espanol", "ESPAÑOL"],
-        ["francais", "FRANÇAIS"],
-        ["deutsch", "DEUTSCH"],
-        ["russian", "PУССКИЙ"],
-        ["portuguese", "PORTUGUÊS"],
-        ["italian", "ITALIANA"],
-        ["internatinal", "Ø"],
-        ["chinese", "中文"]]);
         const member = message.guild.member(user)
-        if (langmap.has(language.toLowerCase))
-        {
-        await member.addRole(langmap.get(language.toLowerCase))
-        message.channel.sendSucess("Role Added!")
-        }
-        if (!language) return message.channel.sendError(
-          !langmap.has(language.toLowerCase)
-              ? "Invalid language!"
-              : "You must provide a language!"
-        )
-        
+        if (!member) return message.channel.sendError("The user is not in the server!")
 
-        
+        const languageInput = args.consume().toLowerCase()
+        const language = LANGUAGE_ROLES[languageInput]
+        if (!language)
+            return message.channel.sendError(
+                languageInput ? "That's not a language!" : "You must provide a language!"
+            )
 
-
-
+        const role = message.guild.roles.cache.find(r => r.name === language)
+        await member.roles.add(role)
+        await message.channel.sendSuccess("Role added!")
     }
 })
