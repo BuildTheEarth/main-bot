7b723960084741e446d26be094af6798b66de1fd
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-17 17:32:41 -0300

    feat(config): create 'emojis' field

--- src/struct/client/ConfigManager.ts
+++ src/struct/client/ConfigManager.ts
@@ -4,11 +4,13 @@ import fs from "fs"
 import Client from "../Client"
 import { SuggestionStatus } from "../../entities/Suggestion"
 import { Action } from "../../entities/ActionLog"
+import { EmojiIdentifierResolvable } from "discord.js"
 
 export type GuildCategories = { main: string; staff: string }
 export type SuggestionCategories = Record<SuggestionStatus, string>
 export type ActionLogCategories = Record<Action, string>
 export type ReactionRole = { [key: string]: { [key: string]: { [key: string]: string } } }
+export type EmojiList = { [key: string]: EmojiIdentifierResolvable }
 export type ColorPalette = { success: string; error: string; info: string }
 export type DatabaseInfo = { host: string; name: string; user: string; pass: string }
 
@@ -22,6 +24,7 @@ export default class ConfigManager implements Config {
     guilds: GuildCategories
     suggestions: GuildCategories & { discussion: GuildCategories }
     reactionRoles: ReactionRole
+    emojis: EmojiList
     colors: ColorPalette & { suggestions: SuggestionCategories }
     assets: { suggestions: SuggestionCategories; cases: ActionLogCategories }
     database: DatabaseInfo
@@ -39,6 +42,9 @@ export default class ConfigManager implements Config {
                 this.client.logger.error(`Failed to read config.yml: ${e.message}`)
                 process.exit(1)
             })
+        for (const [name, resolvable] of Object.entries(config.emojis))
+            if (typeof resolvable === "string" && resolvable.length === 18)
+                config.emojis[name] = this.client.emojis.cache.get(resolvable as string)
 
         for (const [key, value] of Object.entries(config)) this[key] = value
     }
@@ -61,6 +67,7 @@ export type Config = {
     guilds: GuildCategories
     suggestions: GuildCategories & { discussion: GuildCategories }
     reactionRoles: ReactionRole
+    emojis: EmojiList
     colors: ColorPalette & { suggestions: SuggestionCategories }
     assets: { suggestions: SuggestionCategories; cases: ActionLogCategories }
     database: DatabaseInfo
