bf4c86b9f1c1c4a64a6d86f01062a99a1a9399cf
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-17 20:45:57 -0300

    feat(entities): create ModerationNote

---
+++ src/entities/ModerationNote.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, BaseEntity } from "typeorm"
+import SnowflakePrimaryColumn from "./decorators/SnowflakePrimaryColumn"
+
+@Entity({ name: "moderation_notes" })
+export default class ModerationNote extends BaseEntity {
+    @SnowflakePrimaryColumn()
+    member: string
+
+    @Column({ length: 1024 })
+    body: string
+
+    @CreateDateColumn({ name: "created_at" })
+    createdAt: Date
+
+    @UpdateDateColumn({ name: "updated_at" })
+    updatedAt: Date
+
+    @Column("simple-array")
+    updaters: string[]
+}
