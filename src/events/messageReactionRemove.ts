4db8835106b90a014e25306699a2b36abbdb758e
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-18 13:13:34 -0300

    style(events/messageReaction*): format

--- src/events/messageReactionRemove.ts
+++ src/events/messageReactionRemove.ts
@@ -10,10 +10,8 @@ export default async function messageReactionAdd(
     const channel = this.config.reactionRoles?.[reaction.message.channel.id]
     const role = channel?.[reaction.message.id]?.[reaction.emoji.name]
     const guild = reaction.message.guild
-    var member 
-    if(guild) {
-        member = await guild.members.fetch({ user, cache: true })
+    if (guild) {
+        const member = await guild.members.fetch({ user, cache: true })
         await member.roles.remove(role).catch(noop)
     }
-    
 }
