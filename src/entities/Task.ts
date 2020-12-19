b5cb09a1f8f780b7294bb67289c7e6669405199f
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-19 18:11:14 -0300

    feat(entities): create Task entity

---
+++ src/entities/Task.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from "typeorm"
+
+export type TaskStatus = "in-progress" | "abandoned" | "done" | "reported" | "hidden"
+
+@Entity({ name: "tasks" })
+export default class Task extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    @Column({ length: 2048 })
+    description: string
+
+    @Column("simple-array")
+    assignees: string[]
+
+    @Column()
+    status: TaskStatus
+}
