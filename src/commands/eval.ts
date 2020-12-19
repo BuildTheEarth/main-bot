5d7eb481a7545005a747e80f6f4fc86e6b2e4583
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-18 22:53:47 -0300

    refactor(commands/eval): import Everything for easier evaling

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,9 +1,30 @@
-import Client from "../struct/Client"
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Message from "../struct/discord/Message"
+
+import ActionLog from "../entities/ActionLog"
+import ModerationNote from "../entities/ModerationNote"
+import ModpackImage from "../entities/ModpackImage"
+import Snippet from "../entities/Snippet"
+import Suggestion from "../entities/Suggestion"
+import TimedPunishment from "../entities/TimedPunishment"
+
+import CommandList from "../struct/client/CommandList"
+import ConfigManager from "../struct/client/ConfigManager"
+import EventList from "../struct/client/EventList"
+
 import Args from "../struct/Args"
+import Client from "../struct/Client"
 import Command from "../struct/Command"
+
+import formatPunishmentTime from "../util/formatPunishmentTime"
+import formatUTCDate from "../util/formatUTCDate"
+import humanizeConstant from "../util/humanizeConstant"
+import isURL from "../util/isURL"
+import loadDir from "../util/loadDir"
+import noop from "../util/noop"
 import Roles from "../util/roles"
 import truncateString from "../util/truncateString"
+/* eslint-enable @typescript-eslint/no-unused-vars */
 
 export default new Command({
     name: "eval",
