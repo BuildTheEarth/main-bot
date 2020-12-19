b2beb847dca7dcdaf08e1e4f18949944d45361c7
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-18 22:54:16 -0300

    feat: create data, userFlags

---
+++ src/data/userFlags.ts
@@ -0,0 +1,8 @@
+export default {
+    BUGHUNTER_LEVEL_1: "Bug Hunter (Level 1)",
+    BUGHUNTER_LEVEL_2: "Bug Hunter (Level 2)",
+    HYPESQUAD_EVENTS: "HypeSquad Events",
+    HOUSE_BRAVERY: "HypeSquad Bravery",
+    HOUSE_BRILLIANCE: "HypeSquad Brilliance",
+    HOUSE_BALANCE: "HypeSquad Balance"
+}
