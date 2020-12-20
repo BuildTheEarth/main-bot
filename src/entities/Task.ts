3452dcfe2e563ddf465eecfe4c9f5d10aad054f8
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-20 15:02:26 -0300

    feat(entities/Task): export VALID_STATUSES array

--- src/entities/Task.ts
+++ src/entities/Task.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from "typeorm"
 
+export const VALID_STATUSES = ["in-progress", "abandoned", "done", "reported", "hidden"]
 export type TaskStatus = "in-progress" | "abandoned" | "done" | "reported" | "hidden"
 
 @Entity({ name: "tasks" })
