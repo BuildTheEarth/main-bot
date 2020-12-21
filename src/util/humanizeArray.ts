5057e2b2fb0728b9e4d78da039336127b69263c6
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-21 20:45:47 -0300

    feat(util): create humanizeArray()

---
+++ src/util/humanizeArray.ts
@@ -0,0 +1,12 @@
+export default function humanizeArray(
+    array: unknown[],
+    code: boolean = true,
+    conjunction: string = "or",
+    comma: string = ", "
+): string {
+    array = array.map(String)
+    if (code) array = array.map(value => `\`${value}\``)
+    const lastJoiner = comma + conjunction + " "
+    const last = array[array.length - 1]
+    return array.slice(0, array.length - 1).join(comma) + lastJoiner + last
+}
