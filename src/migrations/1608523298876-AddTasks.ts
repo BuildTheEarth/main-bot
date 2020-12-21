e28428cec08485f0bbab8a83308590abafb0bb7d
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-21 01:05:16 -0300

    migration: create AddTasks migration

---
+++ src/migrations/1608523298876-AddTasks.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddTasks1608523298876 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const exists = await queryRunner.hasTable("tasks")
+        if (!exists)
+            await queryRunner.query(`
+CREATE TABLE \`tasks\` (
+    \`id\` int NOT NULL AUTO_INCREMENT,
+    \`title\` varchar(255) NOT NULL,
+    \`description\` varchar(2048) NOT NULL,
+    \`assignees\` text NOT NULL,
+    \`status\` varchar(255) DEFAULT NULL,
+    \`creator\` varchar(18) NOT NULL,
+    PRIMARY KEY (\`id\`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("tasks", true)
+    }
+}
