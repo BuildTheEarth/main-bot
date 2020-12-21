a5b234f3a8f1740b69598e01f2a6f4d2fa564e9b
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-21 01:32:04 -0300

    feat(events/messageReactionAdd): add weekly update publishing (fix #41)

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -1,5 +1,7 @@
 import Discord from "discord.js"
 import Client from "../struct/Client"
+import GuildMember from "../struct/discord/GuildMember"
+import Roles from "../util/roles"
 import noop from "../util/noop"
 
 export default async function messageReactionAdd(
@@ -11,7 +13,32 @@ export default async function messageReactionAdd(
     const role = channel?.[reaction.message.id]?.[reaction.emoji.name]
     const guild = reaction.message.guild
     if (guild) {
-        const member = await guild.members.fetch({ user, cache: true }).catch(() => null)
+        const member: GuildMember = await guild.members
+            .fetch({ user, cache: true })
+            .catch(() => null)
         if (member && role) await member.roles.add(role).catch(noop)
+        const channel = reaction.message.channel as Discord.TextChannel
+
+        if (
+            guild.id === this.config.guilds.staff &&
+            channel.name === "weekly-updates" &&
+            reaction.emoji.name === "📣" &&
+            member.hasStaffPermission(Roles.MANAGER)
+        ) {
+            await reaction.users.remove(user)
+            let update = reaction.message.content
+            if (reaction.message.author.id !== this.user.id)
+                update = `Weekly update from ${reaction.message.author}:\n\n` + update
+
+            const updates = this.channels.cache.find(
+                channel =>
+                    // @ts-ignore
+                    channel.name === "updates" &&
+                    // @ts-ignore
+                    channel.guild?.id === this.config.guilds.main
+            ) as Discord.TextChannel
+
+            await updates.send(update)
+        }
     }
 }
