3e10d22474b8be4576ddc6e7e5a5c912f51d77cc
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-25 17:36:21 -0300

    fix(util/humanizeArray): fix single-value array joining

--- src/util/humanizeArray.ts
+++ src/util/humanizeArray.ts
@@ -6,7 +6,9 @@ export default function humanizeArray(
 ): string {
     array = array.map(String)
     if (code) array = array.map(value => `\`${value}\``)
+
     const lastJoiner = comma + conjunction + " "
     const last = array[array.length - 1]
+    if (array.length === 1) return last as string
     return array.slice(0, array.length - 1).join(comma) + lastJoiner + last
 }
