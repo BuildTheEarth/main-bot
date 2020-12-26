90e49c46c0e2c78c82a6c1f69feaa0f34baaefc0
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-25 23:35:57 -0300

    fix(util/patchedISO6391): don't depend on iso-639-1's internal data

--- src/util/patchedISO6391.ts
+++ src/util/patchedISO6391.ts
@@ -1,64 +1,56 @@
-import LANGUAGES_LIST from "iso-639-1/src/data"
-
-delete LANGUAGES_LIST.zh
-Object.assign(LANGUAGES_LIST, {
-    "zh-s": {
-        name: "Simplified Chinese",
-        nativeName: "简体中文"
-    },
-    "zh-t": {
-        name: "Traditional Chinese",
-        nativeName: "繁體中文"
-    }
-})
+import ISO6391 from "iso-639-1"
 
 // this is an extension of ISO 369-1 which adds "zh-s" and "zh-t" (requested by the Translation Team)
-// the code below is copied directly from the iso-639-1 module (with type declarations added).
-export default class ISO6391 {
-    static getLanguages(codes: string[] = []): IndependentLanguageData[] {
+// data.js isn't accessible (because it's an ES module or whatever), so we'll have to patch
+// on top of the original methods (instead of copying them directly).
+
+// also, all of this is kind of intrinsically a mess, so TODO: find a better way to do... this
+export default class PatchedISO6391 {
+    static getLanguages(codes = []): IndependentLanguageData[] {
         return codes.map(code => ({
             code,
-            name: ISO6391.getName(code),
-            nativeName: ISO6391.getNativeName(code)
+            name: this.getName(code),
+            nativeName: this.getNativeName(code)
         }))
     }
 
     static getName(code: string): string {
-        return ISO6391.validate(code) ? LANGUAGES_LIST[code].name : ""
+        if (removed.includes(code)) return ""
+        return ISO6391.getName(code) || extra[code]?.name || ""
     }
 
-    static getAllNames(): LanguageData[] {
-        // @ts-ignore
-        return Object.values(LANGUAGES_LIST).map(l => l.name)
+    static getAllNames(): string[] {
+        return ISO6391.getAllNames().concat(Object.values(extra).map(lang => lang.name))
     }
 
     static getNativeName(code: string): string {
-        return ISO6391.validate(code) ? LANGUAGES_LIST[code].nativeName : ""
+        return ISO6391.getName(code) || extra[code]?.nativeName || ""
     }
 
     static getAllNativeNames(): string[] {
-        // @ts-ignore
-        return Object.values(LANGUAGES_LIST).map(l => l.nativeName)
+        const extraNativeNames = Object.values(extra).map(lang => lang.nativeName)
+        return ISO6391.getAllNativeNames().concat(extraNativeNames)
     }
 
     static getCode(name: string): string {
-        const code = Object.keys(LANGUAGES_LIST).find(code => {
-            const language = LANGUAGES_LIST[code]
-            return (
-                language.name.toLowerCase() === name.toLowerCase() ||
-                language.nativeName.toLowerCase() === name.toLowerCase()
-            )
-        })
-        return code || ""
+        const base = ISO6391.getCode(name)
+        const extended = Object.keys(extra).find(
+            code =>
+                extra[code].name.toLowerCase() === name.toLowerCase() ||
+                extra[code].nativeName.toLowerCase() === name.toLowerCase()
+        )
+
+        if (removed.includes(base || extended)) return ""
+        return base || extended || ""
     }
 
     static getAllCodes(): string[] {
-        return Object.keys(LANGUAGES_LIST)
+        return ISO6391.getAllCodes().concat(Object.keys(extra))
     }
 
     static validate(code: string): boolean {
-        // eslint-disable-next-line no-prototype-builtins
-        return LANGUAGES_LIST.hasOwnProperty(code)
+        if (removed.includes(code)) return false
+        return ISO6391.validate(code) || !!extra[code]
     }
 }
 
@@ -70,3 +62,16 @@ export interface LanguageData {
 export interface IndependentLanguageData extends LanguageData {
     code: string
 }
+
+const extra = {
+    "zh-s": {
+        name: "Simplified Chinese",
+        nativeName: "简体中文"
+    },
+    "zh-t": {
+        name: "Traditional Chinese",
+        nativeName: "繁體中文"
+    }
+}
+
+const removed = ["zh"]
