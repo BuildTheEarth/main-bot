af81571a9115341d58e990fee193de778b6fcb55
cAttte <26514199+cAttte@users.noreply.github.com>
2021-01-08 18:59:47 -0300

    refactor(commands/eval): use stringifyAnything(), 'use' unused imports, allow 'await' keyword, redact credentials

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Message from "../struct/discord/Message"
+import Args from "../struct/Args"
+import Client from "../struct/Client"
+import Command from "../struct/Command"
 
 import ActionLog from "../entities/ActionLog"
 import ModerationNote from "../entities/ModerationNote"
@@ -12,10 +15,6 @@ import CommandList from "../struct/client/CommandList"
 import ConfigManager from "../struct/client/ConfigManager"
 import EventList from "../struct/client/EventList"
 
-import Args from "../struct/Args"
-import Client from "../struct/Client"
-import Command from "../struct/Command"
-
 import formatPunishmentTime from "../util/formatPunishmentTime"
 import formatUTCDate from "../util/formatUTCDate"
 import humanizeConstant from "../util/humanizeConstant"
@@ -23,8 +22,13 @@ import isURL from "../util/isURL"
 import loadDir from "../util/loadDir"
 import noop from "../util/noop"
 import Roles from "../util/roles"
+import stringifyAnything from "../util/stringifyAnything"
 import truncateString from "../util/truncateString"
 /* eslint-enable @typescript-eslint/no-unused-vars */
+
+// prettier-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _ = { Args, Client, Command, ActionLog, ModerationNote, ModpackImage, Snippet, Suggestion, TimedPunishment, CommandList, ConfigManager, EventList, formatPunishmentTime, formatUTCDate, humanizeConstant, isURL, loadDir, noop, Roles, stringifyAnything, truncateString }
 
 export default new Command({
     name: "eval",
@@ -36,13 +40,23 @@ export default new Command({
         const code = args.removeCodeblock()
 
         try {
-            const out = String(await eval(code)) || "\u200B"
+            const wrapped = code.includes("await") ? `(async () => {${code}})()` : code
+
+            const tokenRegex = new RegExp(client.token, "g")
+            const modpackRegex = new RegExp(client.config.modpack, "g")
+            const pwRegex = new RegExp(client.config.database.pass, "g")
+            const out = stringifyAnything(await eval(wrapped))
+                .replace(tokenRegex, "")
+                .replace(modpackRegex, "")
+                .replace(pwRegex, "")
+
             message.channel.sendSuccess({
                 author: { name: "Output" },
                 description: `\`\`\`js\n${truncateString(out, 1990)}\n\`\`\``
             })
         } catch (error) {
-            const err = error.message || "\u200B"
+            const err = `${error.name || "Error"}: ${error.message}`
+
             message.channel.sendError({
                 author: { name: "Error" },
                 description: `\`\`\`${truncateString(err, 1994)}\`\`\``
