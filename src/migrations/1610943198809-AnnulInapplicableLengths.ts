be397e37a78172d22a84f0d7e1de648c854bef2f
cAttte <26514199+cAttte@users.noreply.github.com>
2021-01-18 01:40:33 -0300

    migration: create AnnulInapplicableLengths migration

---
+++ src/migrations/1610943198809-AnnulInapplicableLengths.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+// this isn't really a schema change,
+// it just sets the length of non-timed action logs (such as warns)
+// to `null` instead of `0`, as that would cause them to display as "permanent"
+export class AnnulInapplicableLengths1610943198809 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            "UPDATE action_logs SET length = NULL WHERE action NOT IN ('mute', 'ban')"
+        )
+    }
+
+    public async down(): Promise<void> {
+        // ...
+    }
+}
