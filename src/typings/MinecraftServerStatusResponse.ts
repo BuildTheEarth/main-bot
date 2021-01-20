07829d4e9f34a611b222b90f4ecc4c08abb66699
cAttte <26514199+cAttte@users.noreply.github.com>
2021-01-20 09:14:02 -0300

    feat(typings): create MinecraftServerStatusResponse interface

---
+++ src/typings/MinecraftServerStatusResponse.ts
@@ -0,0 +1,43 @@
+// https://api.mcsrvstat.us/
+export default interface MinecraftServerStatusResponse {
+    online: boolean
+    ip: string
+    port: number
+    debug: {
+        ping: boolean
+        query: boolean
+        srv: boolean
+        querymismatch: boolean
+        ipinsrv: boolean
+        cnameinsrv: boolean
+        animatedmotd: boolean
+        cachetime: number
+    }
+    motd: FormattedText
+    players: {
+        online: number
+        max: number
+        list?: string[]
+        uuid?: { [name: string]: string }
+    }
+    version: string
+    protocol?: number
+    hostname?: string
+    icon?: string
+    software?: string
+    map: string
+    plugins?: AddonList
+    mods?: AddonList
+    info?: FormattedText
+}
+
+export interface FormattedText {
+    raw: string[]
+    clean: string[]
+    html: string[]
+}
+
+export interface AddonList {
+    names: string[]
+    raw: string[]
+}
