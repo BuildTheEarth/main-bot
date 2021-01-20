60c3193f925e5269c6b45e6a4272b1f9aefc0e64
cAttte <26514199+cAttte@users.noreply.github.com>
2021-01-20 11:05:25 -0300

    feat(commands): create rule command

---
+++ src/commands/rule.ts
@@ -0,0 +1,30 @@
+import Client from "../struct/Client"
+import Message from "../struct/discord/Message"
+import Args from "../struct/Args"
+import Command from "../struct/Command"
+import Roles from "../util/roles"
+
+export default new Command({
+    name: "rule",
+    aliases: [],
+    description: "Get a rule's text.",
+    permission: Roles.ANY,
+    usage: "<number | text>",
+    async run(this: Command, client: Client, message: Message, args: Args) {
+        const query = args.consume().toLowerCase()
+        const number = Number(query)
+        let rule: string
+
+        if (Number.isInteger(number)) {
+            const count = client.config.rules.length
+            if (number > count || number === 0)
+                return message.channel.sendError(`There are only ${count} rules.`)
+            rule = client.config.rules[number - 1]
+        } else {
+            rule = client.config.rules.find(rule => rule.toLowerCase().includes(query))
+            if (!rule) return message.channel.sendError(`Couldn't find that rule.`)
+        }
+
+        return message.channel.send(`>>> ${rule}`)
+    }
+})
