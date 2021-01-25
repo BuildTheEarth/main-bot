744259a9822cafbc002df71f912b04047bb96193
cAttte <26514199+cAttte@users.noreply.github.com>
2021-01-24 21:22:05 -0300

    feat(entities): create BannerImage entity

---
+++ src/entities/BannerImage.ts
@@ -0,0 +1,32 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    DeleteDateColumn,
+    BaseEntity
+} from "typeorm"
+import SnowflakeColumn from "./decorators/SnowflakeColumn"
+
+@Entity({ name: "banner_images" })
+export default class BannerImage extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    url: string
+
+    @Column("simple-array")
+    builders: string[]
+
+    @Column()
+    location: string
+
+    @Column()
+    description: string
+
+    @SnowflakeColumn()
+    creator: string
+
+    @DeleteDateColumn({ name: "deleted_at" })
+    deletedAt?: Date
+}
