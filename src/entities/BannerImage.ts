83cacb3f6cc0a9d117e56cd8bef89986cb030c0c
cAttte <26514199+cAttte@users.noreply.github.com>
2021-01-24 23:26:26 -0300

    refactor(entities/BannerImage): remove 'creator' column

--- src/entities/BannerImage.ts
+++ src/entities/BannerImage.ts
@@ -5,7 +5,6 @@ import {
     DeleteDateColumn,
     BaseEntity
 } from "typeorm"
-import SnowflakeColumn from "./decorators/SnowflakeColumn"
 
 @Entity({ name: "banner_images" })
 export default class BannerImage extends BaseEntity {
@@ -23,9 +22,6 @@ export default class BannerImage extends BaseEntity {
 
     @Column({ nullable: true })
     description?: string
-
-    @SnowflakeColumn()
-    creator: string
 
     @DeleteDateColumn({ name: "deleted_at" })
     deletedAt?: Date
