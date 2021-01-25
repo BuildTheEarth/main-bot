efd83b11aa8e0e9ad2e2d595e92f79cd58b8ce58
metalcupcake5 <imcoolcom5@gmail.com>
2021-01-24 19:31:09 -0800

    migration(AddSnippetAliases): don't default 'aliases' column

--- src/migrations/1611543319029-AddSnippetAliases.ts
+++ src/migrations/1611543319029-AddSnippetAliases.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class AddSnippetAliases1611543319029 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE snippets ADD aliases text NOT NULL DEFAULT ''")
+        await queryRunner.query("ALTER TABLE snippets ADD aliases text NOT NULL")
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
