e0e34f41b1863af44c28c0bf80acf18d03d49f0f
cAttte <26514199+cAttte@users.noreply.github.com>
2021-02-05 21:14:11 -0300

    migration(AddSnippetAlises): check if exists before adding

--- src/migrations/1611543319029-AddSnippetAliases.ts
+++ src/migrations/1611543319029-AddSnippetAliases.ts
@@ -2,7 +2,8 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class AddSnippetAliases1611543319029 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE snippets ADD aliases text NOT NULL")
+        if (!(await queryRunner.hasColumn("snippets", "aliases")))
+            await queryRunner.query("ALTER TABLE snippets ADD aliases text NOT NULL")
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
