3f9582097fa8ba9054e7fbabfab66a1cae8f5d3a
cAttte <26514199+cAttte@users.noreply.github.com>
2021-02-18 16:44:34 -0300

    migration: create MakeBannerCreditsPlain migration

---
+++ src/migrations/1613675796565-MakeBannerCreditsPlain.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+// data loss time!
+export class MakeBannerCreditsPlain1613675796565 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        if (!(await queryRunner.hasColumn("banner_images", "builders"))) return
+        await queryRunner.dropColumn("banner_images", "builders")
+        await queryRunner.query(
+            "ALTER TABLE banner_images ADD COLUMN credit varchar(255) NOT NULL AFTER url"
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn("banner_images", "credit")
+        await queryRunner.query(
+            "ALTER TABLE banner_images ADD COLUMN builders text NOT NULL AFTER url"
+        )
+    }
+}
