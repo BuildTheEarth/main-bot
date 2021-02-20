a29e1386d9f104c77431b41f6c23367f121d704f
cAttte <26514199+cAttte@users.noreply.github.com>
2021-02-20 17:21:50 -0300

    migration: create AddAdvancedBuilders migration

---
+++ src/migrations/1613848362985-AddAdvancedBuilders.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddAdvancedBuilders1613848362985 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const exists = await queryRunner.hasTable("advanced_builders")
+        if (!exists)
+            await queryRunner.query(`
+CREATE TABLE \`advanced_builders\` (
+    \`builder\` varchar(18) NOT NULL,
+    \`given_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    PRIMARY KEY (\`builder\`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("advanced_builders")
+    }
+}
