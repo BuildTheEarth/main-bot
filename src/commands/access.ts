c8ab8cd42dd0d676a921eae5ced2d1764910b4c6
cAttte <26514199+cAttte@users.noreply.github.com>
2021-02-20 01:33:35 -0300

    feat(commands): create access command

---
+++ src/commands/access.ts
@@ -0,0 +1,25 @@
+import Client from "../struct/Client"
+import Message from "../struct/discord/Message"
+import Args from "../struct/Args"
+import Command from "../struct/Command"
+import Roles from "../util/roles"
+
+export default new Command({
+    name: "access",
+    aliases: [],
+    description: "Enable the Manage Permissions permission for a channel.",
+    permission: Roles.MANAGER,
+    usage: "[channel]",
+    async run(this: Command, _client: Client, message: Message, args: Args) {
+        const channel = await args.consumeChannel()
+        const perms = channel.permissionsFor(message.member)
+        if (!perms.has("VIEW_CHANNEL"))
+            return message.channel.sendError("You can't see that channel.")
+
+        const manager = message.guild.roles.cache.find(role => role.name === "Manager")
+        const reason = `Access requested by ${message.author.tag} (${message.author.id})`
+        await channel.updateOverwrite(manager, { MANAGE_ROLES: true }, reason)
+
+        await message.channel.sendSuccess(`Gave managers permission in ${channel}.`)
+    }
+})
