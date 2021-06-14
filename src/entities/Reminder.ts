4b9c23b9912b8b610fac58326a703291facf1a3f
metalcupcake5 <imcoolcom5@gmail.com>
2021-06-14 00:37:15 -0700

    feat(reminders): reminder entity

---
+++ src/entities/Reminder.ts
@@ -0,0 +1,64 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    BaseEntity,
+    CreateDateColumn
+} from "typeorm"
+import SnowflakeColumn from "./decorators/SnowflakeColumn"
+import Client from "../struct/Client"
+import milliseconds from "./transformers/milliseconds"
+import Discord from "discord.js"
+
+@Entity({ name: "reminders" })
+export default class Reminder extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @SnowflakeColumn()
+    channel: string
+
+    @Column({ length: 1024 })
+    message: string
+
+    @Column({ transformer: milliseconds })
+    interval: number
+
+    @CreateDateColumn({ name: "created_at" })
+    createdAt: Date
+
+    @Column({ default: false })
+    cancelled: boolean
+
+    get end(): Date {
+        return new Date(this.createdAt.getTime() + this.interval)
+    }
+
+    private reminderTimeout: NodeJS.Timeout
+
+    async sendReminder(client: Client): Promise<void> {
+        const channel = client.channels.cache.find(
+            channel =>
+                // @ts-ignore
+                channel.id === this.channel
+        ) as Discord.TextChannel
+        if (!channel) return
+        if(!await (await Reminder.findOne(this.id)).cancelled)
+            channel.send(this.message)
+            this.createdAt = new Date(Date.now())
+            this.schedule(client)
+    }
+
+    schedule(client: Client): void {
+        if (this.interval === 0) return
+        const timeout = this.end.getTime() - Date.now()
+
+        // avoid TimeoutOverflowWarning; reschedule
+        // (2147483647 ms is ~24 days)
+        if (timeout > 2147483647) {
+            this.reminderTimeout = setTimeout(() => this.schedule(client), 2147483647)
+        } else {
+            this.reminderTimeout = setTimeout(() => this.sendReminder(client), timeout)
+        }
+    }
+}
