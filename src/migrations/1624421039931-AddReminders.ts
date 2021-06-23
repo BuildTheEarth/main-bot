bf9c299eac91733fbc5a733f16ef6356a1c33ac8
metalcupcake5 <imcoolcom5@gmail.com>
2021-06-22 21:10:32 -0700

    migration: create AddReminders migration

---
+++ src/migrations/1624421039931-AddReminders.ts
@@ -0,0 +1,25 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddReminders1624421039931 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const exists = await queryRunner.hasTable("reminders")
+        if (!exists)
+            await queryRunner.query(`
+            CREATE TABLE \`reminders\` (
+                \`id\` int(11) NOT NULL AUTO_INCREMENT,
+                \`channel\` varchar(18) NOT NULL,
+                \`message\` varchar(1024) NOT NULL,
+                \`interval\` int(11) NOT NULL,
+                \`created_at\` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+                \`cancelled\` tinyint(4) NOT NULL DEFAULT 0,
+                PRIMARY KEY (\`id\`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("reminders", true)
+    }
+
+}
