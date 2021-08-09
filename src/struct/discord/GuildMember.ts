2ee57533e33df190b1d3930db43946c11b07e3c2
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2021-08-08 21:53:02 -0700

    feat(struct/discord)!: breaking change, since we cannot use extend anymore theese all become utility classes to do operations on the Discord.js equivalet of them

--- src/struct/discord/GuildMember.ts
+++ src/struct/discord/GuildMember.ts
@@ -1,28 +1,34 @@
 import Discord from "discord.js"
-import Client from "../Client"
 import Guild from "./Guild"
 import Roles from "../../util/roles"
 
-export default class GuildMember extends Discord.GuildMember {
-    client: Client
-    guild: Guild
-
-    hasRole(roles: string | string[], botDevBypass = true): boolean {
+export default class GuildMember {
+    static hasRole(
+        user: Discord.GuildMember,
+        roles: string | string[],
+        botDevBypass = true
+    ): boolean {
         if (roles === Roles.ANY) return true
         if (typeof roles === "string") roles = [roles]
         if (botDevBypass) roles.push(Roles.BOT_DEVELOPER)
         for (const role of roles)
-            if (this.roles.cache.find(r => r.name === role)) return true
+            if (user.roles.cache.find(r => r.name === role)) return true
         return false
     }
 
-    async mute(reason: string): Promise<this> {
-        await this.roles.add(this.guild.role(Roles.MUTED), reason)
-        return this
+    static async mute(
+        user: Discord.GuildMember,
+        reason: string
+    ): Promise<Discord.GuildMember> {
+        await user.roles.add(Guild.role(user.guild, Roles.MUTED), reason)
+        return user
     }
 
-    async unmute(reason: string): Promise<this> {
-        await this.roles.remove(this.guild.role(Roles.MUTED), reason)
-        return this
+    static async unmute(
+        user: Discord.GuildMember,
+        reason: string
+    ): Promise<Discord.GuildMember> {
+        await user.roles.remove(Guild.role(user.guild, Roles.MUTED), reason)
+        return user
     }
 }
--- src/struct/discord/Role.ts
+++ src/struct/discord/Role.ts
@@ -1,17 +1,10 @@
 import chalk from "chalk"
 import Discord from "discord.js"
-import Client from "../Client"
-import Guild from "./Guild"
-import GuildMember from "./GuildMember"
 
-export default class Role extends Discord.Role {
-    client: Client
-    guild: Guild
-    members: Discord.Collection<string, GuildMember>
-
-    format(): string {
+export default class Role {
+    static format(role: Discord.Role): string {
         // "#000000" is not black, but the default role color
-        const color = this.hexColor === "#000000" ? chalk.white : chalk.hex(this.hexColor)
-        return color(this.name)
+        const color = role.hexColor === "#000000" ? chalk.white : chalk.hex(role.hexColor)
+        return color(role.name)
     }
 }
