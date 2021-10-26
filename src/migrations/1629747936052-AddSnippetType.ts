37923d59f25dc0f6fe3f9a00d8804b2e58270cca
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2021-10-25 21:38:13 -0700

    formatting(migrations): make look neater

--- src/migrations/1629747936052-AddSnippetType.ts
+++ src/migrations/1629747936052-AddSnippetType.ts
@@ -1,13 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class AddSnippetType1629747936052 implements MigrationInterface {
-
     public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query("ALTER TABLE snippets ADD type VARCHAR(7) NOT NULL DEFAULT 'snippet'")
+        queryRunner.query(
+            "ALTER TABLE snippets ADD type VARCHAR(7) NOT NULL DEFAULT 'snippet'"
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         queryRunner.query("ALTER TABLE snippets DROP COLUMN type") // Not even going to bother making this work with sqlite
     }
-
 }
