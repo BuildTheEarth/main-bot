8f0d38c4835db592cd911cb9094f6d0d95511911
Xbox Bedrock <xboxbedrock@gmail.com>
2022-02-04 22:15:03 -0800

    More placeholders code

--- src/struct/client/PlaceholderHandler.ts
+++ src/struct/client/PlaceholderHandler.ts
@@ -1,5 +1,8 @@
+import _ from "lodash"
 import Placeholder from "../../entities/Placeholder"
 import Client from "../Client"
+import iso6391 from "./iso6391"
+import trimSides from "../../util/trimSides"
 
 export default class PlaceholderHandler {
     client: Client
@@ -9,4 +12,55 @@ export default class PlaceholderHandler {
     }
 
     cache = new Map<string, Placeholder>()
+
+    async addPlaceholder(name: string, language: string, body: string) {
+        if (this.cache.has(name + " " + language)) return
+        if (!iso6391.validate(language)) return
+
+        const placeholder = new Placeholder()
+
+        placeholder.name = name
+        placeholder.language = language
+        placeholder.body = body
+
+        await placeholder.save()
+
+        this.cache[name + " " + language] = placeholder
+    }
+
+    async editPlaceholder(name: string, language: string, newBody: string) {
+        if (this.cache.has(name + " " + language)) return
+        if (!iso6391.validate(language)) return
+
+        const placeholder = await Placeholder.findOne({name: name, language: language})
+
+        if (placeholder) {
+            placeholder.body = newBody
+            await placeholder.save()
+        }
+        this.cache[name + " " + language] = placeholder
+    }
+
+    async deletePlaceholder(name: string, language: string) {
+        if (this.cache.has(name + " " + language)) return
+        if (!iso6391.validate(language)) return
+
+        const placeholder = await Placeholder.findOne({name: name, language: language})
+
+        if (placeholder) {
+            await placeholder.remove()
+        }
+        delete this.cache[name + " " + language]
+    }
+
+    replacePlaceholders(text: string) {
+        return text.replaceAll(/{{.+}}/g, (substring: string, ...args: any[]): string => {
+            let trimmedString = trimSides(substring, "{{", "}}")
+            if (trimmedString.split(" ").length > 2) return substring
+            if (trimmedString.split(" ").length == 1) trimmedString += " en"
+
+            return this.cache[trimmedString] || substring
+        })
+    }
 }
+
---
+++ src/util/trimSides.ts
@@ -0,0 +1,5 @@
+import _ from "lodash";
+
+export default function trimSides(text: string, startTrim: string, endTrim: string): string {
+    return _.trimStart(_.trimEnd(text, endTrim), startTrim)
+}
