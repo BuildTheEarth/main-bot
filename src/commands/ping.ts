9988d49e456c954068125eb18dd11520ac349a29
Xbox <68715625+XboxBedrock@users.noreply.github.com>
2022-02-14 18:30:25 -0800

    🏓Ping Pong Bing Bong Ding Dong

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -6,13 +6,13 @@ import Args from "../struct/Args"
 
 export default new Command({
     name: "ping",
-    aliases: ["bing"],
+    aliases: ["bing", "ding"],
     description: "Ping pong!",
     permission: Roles.ANY,
     async run(this: Command, client: Client, message: CommandMessage, args: Args) {
         await message.continue()
         await message.send({
-            content: `:ping_pong: ${args.command === "bing" ? "B" : "P"}ong! **${
+            content: `🏓 ${args.command === "bing" ? "B" : (args.command === "ding": "D": "P")}ong! **${
                 client.ws.ping
             }ms**.`
         })
