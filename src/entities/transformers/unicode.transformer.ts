70f9485673c8c20994a8b660f1447449fdd6778d
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2022-05-21 22:36:41 -0700

    e

--- src/entities/transformers/unicode.transformer.ts
+++ src/entities/transformers/unicode.transformer.ts
@@ -1,20 +1,12 @@
 import typeorm from "typeorm"
 
+
+//this is a ghost transformer till i get stuff working
 export default <typeorm.ValueTransformer>{
-    from: (value: string): string | null =>
-        value === null
-            ? null
-            : /^<.+>$/.test(value)
-            ? value
-            : String.fromCodePoint(parseInt("0x" + value, 16)),
+    from: (value: string): string | null => {
+        return value
+    },
     to: (value: string): string | null => {
-        const returnVal =
-            value === null
-                ? null
-                : /^<.+>$/.test(value)
-                ? value
-                : value.codePointAt(0)?.toString(16)
-        if (!returnVal) return null
-        return returnVal
+        return value
     }
 }
