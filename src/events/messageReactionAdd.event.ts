11931edd61b8e9ec8ed5e495bd7b843d0771ecda
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2022-09-08 16:38:33 -0700

    e

--- src/events/messageReactionAdd.event.ts
+++ src/events/messageReactionAdd.event.ts
@@ -23,7 +23,7 @@ export default async function messageReactionAdd(
             .catch(() => null)
         if (role?.contingent) {
             const checkRoles = role.checkRoles
-            const hasRoles = member?.roles.cache.map(r => r.id)
+            const hasRoles = member?.roles.cache.map(r => r?.id)
             if (role.type === "none") {
                 if (hasRoles?.some(r => checkRoles.includes(r))) return
             }
