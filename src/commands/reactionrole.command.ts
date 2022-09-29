9a199a373e957f7a3efe8663b9dd196de6fff7f9
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2022-09-28 21:02:29 -0700

    lol

--- src/commands/reactionrole.command.ts
+++ src/commands/reactionrole.command.ts
@@ -9,8 +9,53 @@ export default new Command({
     aliases: ["rr"],
     description: "Manage reaction roles",
     permission: [globalThis.client.roles.MANAGER, globalThis.client.roles.BOT_DEVELOPER],
+    subcommands: [
+        {
+            name: "add",
+            description: "Add a reaction role",
+            args: []
+        },
+        {
+            name: "delete",
+            description: "Delete a reaction role",
+            args: []
+        },
+        {
+            name: "blacklist",
+            description: "Blacklist roles from a reaction roles",
+            group: true,
+            subcommands: [
+                {
+                    name: "add",
+                    description: "Add a blacklisted role",
+                    args: []
+                },
+                {
+                    name: "delete",
+                    description: "Delete a blacklisted role",
+                    args: []
+                },
+            ]
+        },
+        {
+            name: "require",
+            description: "Require roles for a reaction roles",
+            group: true,
+            subcommands: [
+                {
+                    name: "add",
+                    description: "Add a required role",
+                    args: []
+                },
+                {
+                    name: "delete",
+                    description: "Delete a required role",
+                    args: []
+                },
+            ]
+        }
+    ],
     async run(this: Command, client: Client, message: CommandMessage, args: Args) {
-        console.log(client) //debug info
         message.sendError("Will be added soon!")
     }
 })
