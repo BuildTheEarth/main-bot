4dae43deb54affbbddcfa62630e63472da930585
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2022-11-20 22:37:10 -0800

    FIX THE CHARSET ONCE AND FOR ALL

--- src/entities/ModerationMenu.entity.ts
+++ src/entities/ModerationMenu.entity.ts
@@ -647,7 +647,7 @@ export default class ModerationMenu extends typeorm.BaseEntity {
     @SnowflakeColumn()
     message!: string
 
-    @typeorm.Column({ length: 2000 })
+    @typeorm.Column({ length: 4000, charset: "utf8mb4" })
     message_text!: string
 
     @typeorm.Column("text", {
